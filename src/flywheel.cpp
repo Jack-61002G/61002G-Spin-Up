0b70b170f4bc0809d020d48283530a882b42cc42
JackReiman <87156347+HobbitHoleJack@users.noreply.github.com>
2022-10-06 20:39:23 -0700

    flywheel is officially good and smart and cool

--- src/flywheel.cpp
+++ src/flywheel.cpp
@@ -2,33 +2,32 @@
 #include "main.h"
 
 void flywheelpid() {
-  // define variables we need for the tbh controller
-  int target{4000}; // our voltage target
-  double gain{1};
+  int target{50}; // our voltage target
+  double gain{100};
   double error{0};
   int output{0};
-  double tbh{0};
-  double prev_error{0};
 
   while (true) {
     if (master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_L1)) {
       if (flywheel_state == false) {
-        target = 7500;
+        target = 170;
         flywheel_state = !flywheel_state;
       } else {
-        target = 4000;
+        target = 50;
         flywheel_state = !flywheel_state;
       }
     }
 
-    if (spinnyman.getActualVelocity() <= 140) {
-    spinnyman.moveVoltage(target);
-    }else {
-    spinnyman.moveVoltage(3500);
+    error = target - spinnyman.getActualVelocity(); 
+
+    output = (error * gain) + 7500;
+
+    if (output >= 12000){
+      output = 12000;
     }
 
+    spinnyman.moveVoltage(output);
 
-    spinnyman.moveVoltage(target);
     pros::delay(10);
   }
 }

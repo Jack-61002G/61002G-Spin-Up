f5f3cd76a0ce45d43f60060626b7e5825207c674
HobbitHoleJack <Jreiman086@gmail.com>
2022-11-14 18:51:45 -0800

    cata is smar

--- src/cata.h
+++ src/cata.h
@@ -7,16 +7,16 @@ class Catapult {
     public: int doCata() { // true - turn motor    false - stop motor
     
         if(master.get_digital(pros::E_CONTROLLER_DIGITAL_R2)){
-            return true;
             cataState = false;
+            return true;
 
         } else if ((limitButton.get_value() == false) && (cataState == false)) { 
             //move catapult down until its reached loading position
             return true;
-            cataState = false;
+            
         } else {
-            return false;
             cataState = true;
+            return false;
         }
     }
 
